963230f5bb9aceb76313bfb9c9419ca6a9af1b01
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-02-02 21:04:56 -0500

    the one where i insstalled a bunch of routing and studied async, pretty painless stuff today
    it was just a list of paths  and some well decorated A tags what was really hard to wrap my head around was the parameterization of component logic data fields into url data using methods likse snap param to turn html templates into a tags  taht were procedurally unique i know its possible now but its kind of non intuitive. this branch is ready to merge with master as it is the most advanced working copy and i should remember to create a villains version and all css the villains all unique for this hall web app

--- hero.service.ts
+++ hero.service.ts
@@ -13,6 +13,17 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+
+  //
+  // getHero(id: number): Observable<Hero>{
+  //   this.messageService.add('HeroService fetdhed hero id= ${id}');
+  //   return of(HEROES.find => hero.id === id));
+  // }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
